7a8d741183ac84aec034ee77121d51ee4893ab19
Shinya SUZUKI <minasitawakou@gmail.com>
2016-06-28 18:51:42 +0900

    Bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
 setup(
         name = "hmc_loss",
         packages = find_packages(),
-        version = "0.3.0",
+        version = "0.3.1",
         url = "https://github.com/TaskeHAMANO/hmc_loss",
         discription = "Validation metrics for hierarchical multi-label classification",
         long_description = long_description,
